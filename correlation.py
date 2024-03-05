29b8258f9fd2503db297ef5357b678e8b9a3d2a8
Hugo Moura <hugomoura02@gmail.com>
2024-03-05 14:58:58 +0100

    update for correlation

---
+++ correlation.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# Load the message count dataset
+message_count_df = pd.read_csv('Datasets/Monthly_Message_Count.csv')
+message_count_df['Date'] = pd.to_datetime(message_count_df['Date'], format='%Y-%m-%d')
+
+# Load the sentiment dataset
+sentiment_df = pd.read_csv('Datasets/Monthly_Sentiment.csv')
+sentiment_df['Date'] = pd.to_datetime(sentiment_df['Date'], format='%Y-%m-%d')
+
+# Load the price dataset
+price_df = pd.read_csv('Datasets/Ocean Protocol Historical Data.csv')
+price_df['Date'] = pd.to_datetime(price_df['Date'], format='%m/%d/%Y')  # Adjust the format accordingly
+
+new_users_df = pd.read_csv('Datasets/Monthly_New_Users.csv')
+new_users_df['Date'] = pd.to_datetime(new_users_df['Date'], format='%Y-%m-%d')  # Adjust the format accordingly
+
+# Merge datasets on 'Date'
+merged_df = pd.merge(message_count_df, sentiment_df, on='Date', how='inner')
+merged_df = pd.merge(merged_df, price_df, on='Date', how='inner')
+merged_df = pd.merge(merged_df, new_users_df, on='Date', how='inner')
+
+
+# Calculate correlation between different variables
+correlation_price_message = merged_df['MessageCount'].corr(merged_df['Price'])
+correlation_price_sentiment = merged_df['mean'].corr(merged_df['Price'])
+correlation_price_new_users = merged_df['NewUsersCount'].corr(merged_df['Price'])
+
+print(f'Correlation between message count and price: {correlation_price_message}')
+print(f'Correlation between sentiment and price: {correlation_price_sentiment}')
+print(f'Correlation between new users and price: {correlation_price_new_users}')
---
+++ numberofnewusers.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load your Discord dataset
+df = pd.read_csv('Datasets/Ocean Discord Data Challenge Dataset.csv')
+
+df['Content'].fillna('N/A', inplace=True)
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M %p')
+
+# Filter messages containing "Joined the server."
+new_users_df = df[df['Content'].str.contains("Joined the server.")]
+
+# Extract month and year from the 'Date' column
+new_users_df['MonthYear'] = new_users_df['Date'].dt.to_period("M")
+
+new_users_df['MonthYear'] = new_users_df['MonthYear'].dt.to_timestamp()
+
+# Count the number of new users per month
+new_users_count_per_month = new_users_df.groupby('MonthYear').size().reset_index(name='NewUsersCount')
+
+new_users_count_per_month.to_csv('new_users_count_per_month.csv', index=False)
+# Print the result
+print(new_users_count_per_month)
+
+total_new_users_count = new_users_count_per_month['NewUsersCount'].sum()
+
+# Print the total number of new users
+print("Total number of new users:", total_new_users_count)
+
+plt.figure(figsize=(12, 6))
+plt.bar(new_users_count_per_month['MonthYear'].astype(str), new_users_count_per_month['NewUsersCount'], color='skyblue')
+plt.title('Number of New Users per Month')
+plt.xlabel('Month-Year')
+plt.ylabel('New Users Count')
+plt.xticks(rotation='vertical')
+plt.show()
