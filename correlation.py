84740e699884a5e0ef64f719dc07d7970caacd1e
Hugo Moura <hugomoura02@gmail.com>
2024-03-11 13:07:45 +0100

    correlation fix

--- correlation.py
+++ correlation.py
@@ -1,27 +1,26 @@
 import pandas as pd
 
-# Load dataset
+#main dataset
 message_count_df = pd.read_csv('Datasets/Monthly_Message_Count.csv')
 message_count_df['Date'] = pd.to_datetime(message_count_df['Date'], format='%Y-%m-%d')
 
-# Load the sentiment dataset
+#sentiment dataset
 sentiment_df = pd.read_csv('Datasets/Monthly_Sentiment.csv')
 sentiment_df['Date'] = pd.to_datetime(sentiment_df['Date'], format='%Y-%m-%d')
 
-# Load the price dataset
+#price dataset
 price_df = pd.read_csv('Datasets/Ocean Protocol Historical Data.csv')
 price_df['Date'] = pd.to_datetime(price_df['Date'], format='%m/%d/%Y')  # Adjust the format accordingly
 
 new_users_df = pd.read_csv('Datasets/Monthly_New_Users.csv')
 new_users_df['Date'] = pd.to_datetime(new_users_df['Date'], format='%Y-%m-%d')  # Adjust the format accordingly
 
-# Merging on 'Date'
 merged_df = pd.merge(message_count_df, sentiment_df, on='Date', how='inner')
 merged_df = pd.merge(merged_df, price_df, on='Date', how='inner')
 merged_df = pd.merge(merged_df, new_users_df, on='Date', how='inner')
 
 
-# Calculate correlation between different variables
+# Calculate correlation between diff. variables
 correlation_price_message = merged_df['MessageCount'].corr(merged_df['Price'])
 correlation_price_sentiment = merged_df['mean'].corr(merged_df['Price'])
 correlation_price_new_users = merged_df['NewUsersCount'].corr(merged_df['Price'])
