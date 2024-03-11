e5f62c62874e6edeb5247f63e233db7428b38662
Hugo Moura <hugomoura02@gmail.com>
2024-03-11 15:14:20 +0100

    updates

--- Prediction_model/prediction_messages.py
+++ Prediction_model/prediction_messages.py
@@ -1,18 +1,18 @@
+#(c) 2023 SMDS-Studio
 from neuralprophet import NeuralProphet
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_absolute_error
 from matplotlib.dates import YearLocator
 
-sent = pd.read_csv('Datasets/Monthly_Sentiment.csv')
-sent['Date'] = pd.to_datetime(sent['Date'])
-sent = sent[['Date', 'mean']]
-sent.columns = ['ds', 'y']
+data = pd.read_csv('Datasets/Monthly_Message_Count.csv')
+data['Date'] = pd.to_datetime(data['Date'])
+data = data[['Date', 'MessageCount']]
+data.columns = ['ds', 'y']
 
 fig, ax = plt.subplots()
-ax.plot(sent['ds'], sent['y'], label='actual', color='g')
+ax.plot(data['ds'], data['y'], label='actual', color='g')
 
 # Set the locator for the x-axis to show years
 ax.xaxis.set_major_locator(YearLocator())
@@ -21,22 +21,22 @@ ax.xaxis.set_major_locator(YearLocator())
 plt.xticks(rotation=45, ha='right')
 
 plt.legend()
-plt.title('Sentiment Over Time')
+plt.title('Monthly Message Count Over Time')
 plt.xlabel('Date')
 plt.ylabel('Message Count')
 plt.show()
 
 model = NeuralProphet()
-model.fit(sent)
+model.fit(data)
 
-future = model.make_future_dataframe(sent, periods = 33)
+future = model.make_future_dataframe(data, periods = 55)
 
 forecast = model.predict(future)
-actual_prediction = model.predict(sent)
+actual_prediction = model.predict(data)
 
 plt.plot(actual_prediction['ds'], actual_prediction['yhat1'], label = "prediction_Actual", c = 'r')
 plt.plot(forecast['ds'], forecast['yhat1'], label = 'future_prediction', c = 'b')
-plt.plot(sent['ds'], sent['y'], label = 'actual', c = 'g')
+plt.plot(data['ds'], data['y'], label = 'actual', c = 'g')
 plt.legend()
 plt.title('Prediction')
 plt.show()
@@ -45,13 +45,13 @@ model.plot_components(forecast)
 mae_historical = mean_absolute_error(actual_prediction['y'], actual_prediction['yhat1'])
 print(f"Mean Absolute Error on Historical Data: {mae_historical}")
 
-# Calculate MAE for future predictions
-print("Length of Sentiment Data:", len(sent['y']))
+# Check the lengths of actual and forecast for future predictions
+print("Length of Actual Data:", len(data['y']))
 print("Length of Forecast Data:", len(forecast['yhat1']))
 
-# Ensure that the lengths match before calculating MAE
-if len(sent['y']) == len(forecast['yhat1']):
-    mae_future = mean_absolute_error(sent['y'], forecast['yhat1'][:len(sent['y'])])
+# Calculate MAE for future predictions if lengths match
+if len(data['y']) == len(forecast['yhat1']):
+    mae_future = mean_absolute_error(data['y'], forecast['yhat1'])
     print(f"Mean Absolute Error on Future Data: {mae_future}")
 else:
-    print("Lengths of sentiment and forecast data are inconsistent.")
+    print("Lengths of actual and forecast data are inconsistent.")
--- Prediction_model/prediction_sentiment.py
+++ Prediction_model/prediction_sentiment.py
@@ -1,18 +1,19 @@
+#(c) 2023 SMDS-Studio
 from neuralprophet import NeuralProphet
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error
 from matplotlib.dates import YearLocator
 
-
-data = pd.read_csv('Datasets/Monthly_Message_Count.csv')
-data['Date'] = pd.to_datetime(data['Date'])
-data = data[['Date', 'MessageCount']]
-data.columns = ['ds', 'y']
+sent = pd.read_csv('Datasets/Monthly_Sentiment.csv')
+sent['Date'] = pd.to_datetime(sent['Date'])
+sent = sent[['Date', 'mean']]
+sent.columns = ['ds', 'y']
 
 fig, ax = plt.subplots()
-ax.plot(data['ds'], data['y'], label='actual', color='g')
+ax.plot(sent['ds'], sent['y'], label='actual', color='g')
 
 # Set the locator for the x-axis to show years
 ax.xaxis.set_major_locator(YearLocator())
@@ -21,22 +22,22 @@ ax.xaxis.set_major_locator(YearLocator())
 plt.xticks(rotation=45, ha='right')
 
 plt.legend()
-plt.title('Monthly Message Count Over Time')
+plt.title('Sentiment Over Time')
 plt.xlabel('Date')
 plt.ylabel('Message Count')
 plt.show()
 
 model = NeuralProphet()
-model.fit(data)
+model.fit(sent)
 
-future = model.make_future_dataframe(data, periods = 55)
+future = model.make_future_dataframe(sent, periods = 55)
 
 forecast = model.predict(future)
-actual_prediction = model.predict(data)
+actual_prediction = model.predict(sent)
 
 plt.plot(actual_prediction['ds'], actual_prediction['yhat1'], label = "prediction_Actual", c = 'r')
 plt.plot(forecast['ds'], forecast['yhat1'], label = 'future_prediction', c = 'b')
-plt.plot(data['ds'], data['y'], label = 'actual', c = 'g')
+plt.plot(sent['ds'], sent['y'], label = 'actual', c = 'g')
 plt.legend()
 plt.title('Prediction')
 plt.show()
@@ -45,13 +46,13 @@ model.plot_components(forecast)
 mae_historical = mean_absolute_error(actual_prediction['y'], actual_prediction['yhat1'])
 print(f"Mean Absolute Error on Historical Data: {mae_historical}")
 
-# Check the lengths of actual and forecast for future predictions
-print("Length of Actual Data:", len(data['y']))
+# Calculate MAE for future predictions
+print("Length of Sentiment Data:", len(sent['y']))
 print("Length of Forecast Data:", len(forecast['yhat1']))
 
-# Calculate MAE for future predictions if lengths match
-if len(data['y']) == len(forecast['yhat1']):
-    mae_future = mean_absolute_error(data['y'], forecast['yhat1'])
+# Ensure that the lengths match before calculating MAE
+if len(sent['y']) == len(forecast['yhat1']):
+    mae_future = mean_absolute_error(sent['y'], forecast['yhat1'][:len(sent['y'])])
     print(f"Mean Absolute Error on Future Data: {mae_future}")
 else:
-    print("Lengths of actual and forecast data are inconsistent.")
+    print("Lengths of sentiment and forecast data are inconsistent.")
